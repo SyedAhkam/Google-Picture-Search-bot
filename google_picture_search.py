62594918f0d0cd11ee1ab0137dd659d033f28188
SyedAhkam <52673095+SyedAhkam@users.noreply.github.com>
2020-01-02 15:09:47 +0530

    Update google_picture_search.py

--- google_picture_search.py
+++ google_picture_search.py
@@ -6,24 +6,24 @@ import time
 url = 'https://www.google.com'
 query = input('What do you want to search for?\n') ## Change query to anything
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome() ## Open Chrome
 browser.get(url)
 
 text_field = browser.find_element_by_name('q')
-text_field.send_keys(query)
+text_field.send_keys(query)  ## Search about the query
 text_field.send_keys(Keys.RETURN)
 
 time.sleep(1)
 
 images = browser.find_element_by_link_text('Images')
-images.click()
+images.click()  ## Search for images
 time.sleep(2)
 
 counter = 1
 while True:
-    body = browser.find_element_by_tag_name('body')
+    body = browser.find_element_by_tag_name('body') ## Scroll Down
     body.send_keys(Keys.DOWN)
     time.sleep(2)
-    browser.save_screenshot("screenshot" + str(counter) + ".png")
+    browser.save_screenshot("screenshot" + str(counter) + ".png") ## Capture Screenshot
     print('Saved Screenshot' + str(counter) + '.png')
     counter = counter + 1
